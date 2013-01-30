5344e5eb7e33d8e20e0eee73e1667795029968cc
david raistrick <draistrick@ea.com>
2013-01-30 18:19:53 -0500

    split section config out of the view

---
+++ htdocs/sections.php
@@ -0,0 +1,18 @@
+<?php
+require_once('phplib/Dashboard.php');
+
+$sections = array(
+    'Application' => array(
+        'Deploy' => Dashboard::$DEPLOY_TABS,
+    ),
+    'Operations' => array(
+        'Database' => Dashboard::$DB_TABS,
+        'Network' => Dashboard::$NETWORK_TABS,
+        'Chef' => array(
+            'chef' => '/example_chef.php',
+        ),
+        'Hadoop' => Dashboard::$HADOOP_TABS,
+        'Util' => Dashboard::$TIME_TABS,
+    ),
+);
+
