e58bbd2ee2f5577fb61aa6f4fea00265fa9e3356
david raistrick <draistrick@ea.com>
2013-01-30 19:07:48 -0500

    remove stale file, fix class stuff

--- htdocs/conf/sections.php
+++ htdocs/conf/sections.php
@@ -4,16 +4,16 @@ require_once('dashboards.php');
 
 $sections = array(
     'Application' => array(
-        'Deploy' => Dashboard::$DEPLOY_TABS,
+        'Deploy' => Local_Dashboard::$DEPLOY_TABS,
     ),
     'Operations' => array(
-        'Database' => Dashboard::$DB_TABS,
-        'Network' => Dashboard::$NETWORK_TABS,
+        'Database' => Local_Dashboard::$DB_TABS,
+        'Network' => Local_Dashboard::$NETWORK_TABS,
         'Chef' => array(
-            'chef' => '/example_chef.php',
+            'chef' => 'tabs/example_chef.php',
         ),
-        'Hadoop' => Dashboard::$HADOOP_TABS,
-        'Util' => Dashboard::$TIME_TABS,
+        'Hadoop' => Local_Dashboard::$HADOOP_TABS,
+        'Util' => Local_Dashboard::$TIME_TABS,
     ),
 );
 
