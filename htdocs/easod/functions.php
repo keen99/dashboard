7243183132af1e1a9a2a659ee2be0faa428008ad
david raistrick <draistrick@ea.com>
2013-02-04 15:20:46 -0500

    timer functions, test function, and most importantly remove wasted loops for data we never used 17s to 1s

--- htdocs/easod/functions.php
+++ htdocs/easod/functions.php
@@ -46,6 +46,26 @@ $COLORS = array(
     
 
 
+function startTimer() {
+	global $starttime;
+  $mtime = microtime(); 
+  $mtime = explode(' ', $mtime); 
+  $mtime = $mtime[1] + $mtime[0]; 
+  $starttime = $mtime; 
+
+}
+
+
+function printTimer($prefix='Timer') {
+	global $starttime;
+  $mtime = microtime(); 
+  $mtime = explode(" ", $mtime);
+  $mtime = $mtime[1] + $mtime[0]; 
+  $endtime = $mtime; 
+  $totaltime = ($endtime - $starttime); 
+  echo $prefix . ' Elapsed time: ' .$totaltime. ' seconds<br>';
+}
+
 function fetchGraphiteData() {
 //todo - add some caching here
 	global $graphite_server;
@@ -99,64 +119,117 @@ function createGraphsFromTemplates($name, $groupby="service") {
 		$suffixpattern=$graphTemplate["$name"]['suffixpattern'];
 	
 	
+		$matches = preg_grep("/$prefixpattern\.$hostpattern.*\.$servicepattern\.$suffixpattern/", $data);
+		$matches = array_unique($matches);
+		
+		if (! isset($graphs) )
+		 $graphs = array();
+	
+		$i = 0;
+		foreach ($matches as $value) {
+			
+			//		echo "in here for $value and $i<br>";
+			unset($graphdata);
+			preg_match("/$prefixpattern\.$hostpattern.*\.$servicepattern\.$suffixpattern/", $value, $graphdata);
+//echo "<pre>";
+//print_r($graphdata);
+			if (! isset($graphdata[0]) ) {
+			 echo "we failed to set graphdata, what happened...value $value<br>";
+			}
+			$graphtarget=$graphdata[0];
+			$graphprefix=$graphdata[1];
+			$graphhost=$graphdata[2];
+			$graphservice=$graphdata[3];
+			$graphsuffix=$graphdata[4];
+
+
+
+
+
+			if (! isset($colorid) )
+			  $colorid = 0;
+			
+			if ( $colorid > count($COLORS)-1 )
+			 $colorid = 0;					
+
+			if ( is_array($graphTemplate["$name"]["metrics"]) ) {
+				$metrics = $graphTemplate["$name"]["metrics"];
+			} else {
+				$metrics = array("cactiStyle(alias(keepLastValue($value), \"$graphservice\"))");
+			}
+			$newgraph = 
+				array(
+					'type' => 'graphite',
+					'title' => "$graphhost - $graphservice",
+					'metrics' => $metrics, 
+					'show_legend' => 0,
+					'show_html_legend' => 1,
+					'show_copy_url' => 0,
+					'height' => '120',
+					'color' => $COLORS[$colorid],
+		//			'width' => '200',
+		//			'area_mode' => 'first',
+		//			'line_mode' => 'connected',
+		//			'line_mode' => 'slope',
+		//			'line_mode' => 'staircase',
+		//			'vtitle' => 'vtitle is not fun',
+		//			'is_ajax' => 1,
+		//			'is_pie_chart' => 1,
+			);
+
+			if ( "$groupby" == "host" ) {
+				$graphs["$graphhost"]["$graphservice"] = $newgraph;
+			} elseif ( "$groupby" == "service" ) {
+				$graphs["$graphservice"]["$graphhost"] = $newgraph;
+			} else { 
+				echo "unknown groupby for this graph template - $name";
+				return 1;
+			}
+
+			$colorid++;
+			$i++;
+		}
+	}
+	/*
+	echo "<pre>";
+	print_r($graphs);
+	echo "</pre>";
+	*/
+}
+
+
+
+
+
+
+
+
+
+function createGraphsFromTemplatesAggregate($name, $groupby="service") {
+	global $graphTemplate, $COLORS, $graphs; 
+
+
+	if (! is_array($graphTemplate["$name"]) ) {
+		echo "sorry, can't find graph template named $name<br>";
+		exit;
+	} else {
+
+		$data=fetchGraphiteData();
+
+printTimer('postFetch');
+	
+		$prefixpattern=$graphTemplate["$name"]['prefixpattern'];
+		$hostpattern=$graphTemplate["$name"]['hostpattern'];
+		$servicepattern=$graphTemplate["$name"]['servicepattern'];
+		$suffixpattern=$graphTemplate["$name"]['suffixpattern'];
+	
+	
 	
 		$p=0;
 		$h=0;
 		$s=0;
 		$ss=0;
-		foreach ( $data as $key => $value ) {
-			preg_match("/$prefixpattern\.$hostpattern.*\.$servicepattern\.$suffixpattern/", $value, $match);
-		
-		//print_r($match);
-		//exit;
-		
-			if ( isset($match[1]) ) {
-				$allprefixes[$p] = $match[1];
-				$p++;
-			}
-				if ( isset($match[2]) ) {
-				$allhosts[$h] = $match[2];
-				$h++;
-			}
-		
-			if ( isset($match[3]) ) {
-				$allservices[$s] = $match[3];
-				$s++;
-			}
-			if ( isset($match[4]) ) {
-				$allsuffixes[$ss] = $match[4];
-				$ss++;
-			}
-		}
-		if ( isset($allhosts) && isset($allservices)  && isset($allprefixes) && isset($allsuffixes) ) {
-			$allhosts = array_unique($allhosts);
-			$allservices = array_unique($allservices);
-			$allprefixes = array_unique($allprefixes);
-			$allsuffixes = array_unique($allsuffixes);
-		} else {
-			echo "sorry, our pattern matching failed. missing hosts/services/prefixes/suffixes.. graphtemplate $name";
-			return 1;
-		}		
-/*
-echo "<pre>";
-echo "prefixes<hr>";
-flush();
-print_r($allprefixes);
-echo "hosts<hr>";
-flush();
-print_r($allhosts);
-echo "services<hr>";
-flush();
-print_r($allservices);
-echo "suffixes<hr>";
-flush();
-print_r($allsuffixes);
-*/
 
-
-		foreach ( $allservices as $key => $value) {
-			
-			//echo "doing $value<hr>";
 
 			$matches = preg_grep("/$prefixpattern\.$hostpattern.*\.$servicepattern\.$suffixpattern/", $data);
 			$matches = array_unique($matches);
@@ -165,6 +238,7 @@ print_r($allsuffixes);
 			 $graphs = array();
 		
 			$i = 0;
+			//for each 
 			foreach ($matches as $value) {
 				
 				//		echo "in here for $value and $i<br>";
@@ -228,11 +302,15 @@ print_r($allsuffixes);
 				$colorid++;
 				$i++;
 			}
-		}
+printTimer('postServices');
 	}
 	/*
 	echo "<pre>";
 	print_r($graphs);
 	echo "</pre>";
 	*/
+
+	echo "<hr>graph count: " . count($graphs) . "<hr><br>";
+
 }
+
--- htdocs/kiwi-rpc.php
+++ htdocs/kiwi-rpc.php
@@ -2,6 +2,9 @@
 require_once 'phplib/Dashboard.php';
 require_once 'easod/functions.php';
 
+
+startTimer();
+
 /** the title used for the page */
 $title = 'kiwi rpcs';
 $namespace="";
@@ -26,14 +29,20 @@ addGraphTemplate(
 	array("keepLastValue(statsd.live-kiwi-app*.kiwi.*.rpc.auction.bid.count.counters.count)")
 );
 
-createGraphsFromTemplates("kiwirpcs", "host");
-//createGraphsFromTemplates("kiwirpcs", "service");
+//createGraphsFromTemplatesAggregate("kiwirpcs", "host");
+//createGraphsFromTemplatesAggregateShort("kiwirpcs", "host");
+
+//createGraphsFromTemplatesAggregate("kiwirpcs", "service");
+//createGraphsFromTemplatesAggregateShort("kiwirpcs", "service");
+
+//createGraphsFromTemplates("kiwirpcs", "host");
+createGraphsFromTemplates("kiwirpcs", "service");
 
 //createGraphsFromTemplates("kiwitest");
 
 
 
+ printTimer('pre template');
+
 /** actually draws the page */
 include 'phplib/template.php';
-
-
