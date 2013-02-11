f546ad9f6e9bee8113827e5c3b3b328e635b32f6
david raistrick <draistrick@ea.com>
2013-02-11 16:15:19 -0500

    working controlmagic

--- htdocs/test-kiwi.php
+++ htdocs/test-kiwi.php
@@ -10,6 +10,11 @@ $title = 'kiwi rpcs';
 $namespace="";
 
 
+$graphTemplate['empty'] = array(
+	'sectiontitle' => 'PICK ONE'
+);
+
+
 $graphTemplate['test template bypass'] = array(
 		'type' => 'graphite'
 		,'sectiontitle' => 'kiwi rpc test counters'
@@ -28,8 +33,8 @@ $graphTemplate['test template bypass'] = array(
 		,'rightaxispattern' => '(rpc\..*\.errors)'
 		,'rightaxisalias' => 'errors'
 // so we probably will need some specific logic around stacks, wen we can'cant use them
-		,'rightaxisfunctions' => 'secondYAxis,stacked,keepLastValue'
-	,'rightaxisfunctions' => 'stacked,keepLastValue'
+//		,'rightaxisfunctions' => 'secondYAxis,stacked,keepLastValue'
+		,'rightaxisfunctions' => 'stacked,keepLastValue'
 
 
 //		,'leftaxispattern' => '(rpc\.blaze\.post\.count)	
@@ -41,6 +46,101 @@ $graphTemplate['test template bypass'] = array(
 );
 
 
+
+$graphTemplate['kiwiallrpccounts'] = array(
+		'sectiontitle' => 'kiwi all rpc counts'
+		,'prefixpattern' => '(^statsd)'
+		,'hostpattern' => '(.*app.*)\.kiwi'
+		,'servicepattern' => '(rpc\..*)'
+		,'suffixpattern' => '(errors\.counters\.count|count\.counters\.count)'
+
+		,'leftaxispattern' => '(rpc\..*\.count)'
+		,'leftaxisalias' => 'count'
+		,'leftaxisfunctions' => 'keepLastValue'
+		,'rightaxispattern' => '(rpc\..*\.errors)'
+		,'rightaxisalias' => 'errors'
+// so we probably will need some specific logic around stacks, wen we can'cant use them
+//		,'rightaxisfunctions' => 'secondYAxis,stacked,keepLastValue'
+		,'rightaxisfunctions' => 'stacked,keepLastValue'		
+);
+
+
+
+$graphTemplate['kiwiallrpcduration'] = array(
+		'sectiontitle' => 'kiwi all rpc durations'
+		,'prefixpattern' => '(^statsd)'
+		,'hostpattern' => '(.*app.*)\.kiwi'
+		,'servicepattern' => '(rpc\..*)'
+//		,'suffixpattern' => '(errors\.counters\.count|count\.counters\.count)'
+		,'suffixpattern' => '(duration\.avg\.counters\.count|duration\.max\.counters\.count)'
+
+
+		,'leftaxispattern' => '(rpc\..*\.duration.avg)'
+		,'leftaxisalias' => 'avg'
+		,'leftaxisfunctions' => 'keepLastValue'
+		,'rightaxispattern' => '(rpc\..*\.duration.max)'
+		,'rightaxisalias' => 'max'
+		,'rightaxisfunctions' => 'keepLastValue'		
+);
+
+
+$additional_controls="";
+//this isn't really good enough to carry stuff forward, we need to limit it so it doesn't goet longer and longer and longer
+//foreach ($_GET as $key => $value) {
+//    $additional_controls .= "<input type='hidden' name='$key' value='$value'/>";
+//}
+
+//$additional_controls .= "<input type='checkbox' name='whichgraph' value='0' />";
+$additional_controls .= "<span class='control'>";
+//default to service
+if ( !isset($_GET['groupby']) ) { $_GET['groupby'] = 'service'; }
+$additional_controls .= Controls::buildRadio('groupby', 'Group By', array('service' => 'Service' , 'host' => 'Host'), $_GET['groupby'] );
+$additional_controls .= "</span><span class='control'>";
+if (! isset($_GET['sum']) ) { $_GET['sum'] = 0; }
+$additional_controls .= Controls::buildCheckbox('sum', 'Sum Items', $_GET['sum'] );
+if (! isset($_GET['agg']) ) { $_GET['agg'] = 0; }
+$additional_controls .= Controls::buildCheckbox('agg', 'Aggregate', $_GET['agg'] );
+$additional_controls .= '</span>';
+$additional_controls .= "<span class='control'>";
+
+
+if ( !isset($_GET['graphtemplate']) ) {
+	$_GET['graphtemplate'] = 'empty';
+}
+
+if ( $_GET['graphtemplate'] === 'empty' ) {
+
+	echo '<div class=notice>please choose a graph template<br></div>';
+	$graphs=array();
+} else {
+	createGraphsFromTemplates($_GET['graphtemplate'], $_GET['groupby'], $_GET['sum'], $_GET['agg']);
+
+}
+
+//$additional_controls .= Controls::buildSelect('selectfun', 'select one fun', array('service' => 'Service' , 'host' => 'Host'), $_GET['elected'] );
+foreach ( $graphTemplate as $key => $value) { $graphTemplates[$key] = $value['sectiontitle'];	}
+$additional_controls .= Controls::buildSelect('graphtemplate', 'Graph Template', $graphTemplates, $_GET['graphtemplate'] );
+
+
+//createGraphsFromTemplates("test template bypass", $_GET['groupby'], $_GET['sum'], $_GET['agg']);
+
+
+/*
+if ( isset($_GET['whichgraph']) ) {
+	echo "got whichgraph ".  $_GET['whichgraph'] . "<br>";
+
+}
+
+if ( isset($_GET['whichgraph']) ) {
+
+	switch($_GET['whichgraph']) {
+		case 0:
+			createGraphsFromTemplates("test template bypass", "service", false, false);
+		case 1:
+			createGraphsFromTemplates("test template bypass", "host", false, false);
+
+	}
+} else {
 
 //1
 //  group by service, each service and each host on it's own graph - err/count split
@@ -62,27 +162,26 @@ $graphTemplate['test template bypass'] = array(
 //createGraphsFromTemplates("test template bypass", "host", false, true);
 //7
 //  group by host, each service and each host on it's own graph - combined errr/count
-createGraphsFromTemplates("test template bypass", "host", true, false);
+//createGraphsFromTemplates("test template bypass", "host", true, false);
 //8
 //  group by host, aggregate each host (all services) onto one graph
 //createGraphsFromTemplates("test template bypass", "host", true, true);
-
+}
+*/
 
 // adds another input under the deploys inputs...
 //$additional_controls="<input type='checkbox' name='hype_deploys' value='true'/><label>Hype All Deploys</label>";
 
 //$hide_deploys=true;
 
-$additional_controls="";
-//this isn't really good enough to carry stuff forward, we need to limit it so it doesn't goet longer and longer and longer
-foreach ($_GET as $key => $value) {
-    $additional_controls .= "<input type='hidden' name='$key' value='$value'/>";
-}
+
 
 //this sets up a tab-navigation between pages.  also adds an "up to home" link
 $tabs = Local_Dashboard::$KIWI_TABS;
 $tab_url = Dashboard::getTabUrl(__FILE__);
 
+
+// actually adds it right before the main graph frame.
 // and adds html to the header
 //$html_for_header=...
 
@@ -101,9 +200,9 @@ function generateBox($graphs) {
 		$sectioncount++;
 		while ($graph = current($graphs[$sectionkey]) ){ 		
 			//this is our graph itself
-//dont show these yet
+//dont show these yet we cant link to them
 //			$graphkey=key($graphs[$sectionkey]);
-			//$boxes.="<a href=#". preg_replace("/[^a-z0-9]/", "", strtolower($graphkey) . ">$graphkey</a><br>";
+//			$boxes.="<a href=#" . preg_replace("/[^a-z0-9]/", "", strtolower($graphkey)) . ">$graphkey</a><br>";
 
 			$graphcount++;
 		next($graphs[$sectionkey]);
@@ -115,6 +214,14 @@ function generateBox($graphs) {
 	$boxcontent="<b>Sections: $sectioncount</b><br>";	
 	$boxcontent.="<b>Graphs: $graphcount</b><hr>";	
 	$boxcontent.=$boxes;
+	$boxcontent='<div id=menu style="background-color:#fff;
+					width:10%;
+					height:auto;
+					font-size:10px;
+					float: left;">
+					' . $boxcontent . '
+					</div>
+					<div id=frame style="width:90%;"">';
 	return $boxcontent;
 };
 
@@ -122,17 +229,9 @@ function generateBox($graphs) {
 
 //exit;
 
-
-$html_for_header='
-<div id=menu style="background-color:#FFD700;
-	width:10%;
-	height:auto;
-	font-size:10px;
-	float: left;">' . generateBox($graphs) . '
-</div>
-<div id=frame style="width:90%;"">
-';
-
+//if (!empty($graphs)) {
+$html_for_header=generateBox($graphs);
+//}
 
 
 
